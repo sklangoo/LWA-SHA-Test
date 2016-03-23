99f17ecd5b0b43831486f202ed5a75e8214e490a
sklangoo <langoos@amazon.com>
2016-03-23 12:45:32 -0700

    Update shaTest.js

--- scripts/shaTest.js
+++ scripts/shaTest.js
@@ -1,3 +1,11 @@
+/**
+ * Login with Amazon - SHA 256 compatibility test scripts
+ *
+ * @category    Amazon
+ * @package     shaTest.js
+ * @copyright   Copyright (c) 2016 Amazon.com
+ * @license     http://opensource.org/licenses/Apache-2.0  Apache License, Version 2.0
+ */
 var https = require('https');
 
 https.get('https://api.sandbox.amazon.com', function(res) {

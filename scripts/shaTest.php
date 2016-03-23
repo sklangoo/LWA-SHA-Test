d79ddc9066d9189e98eb223b3011a4649489b212
sklangoo <langoos@amazon.com>
2016-03-23 12:46:00 -0700

    Update shaTest.php

--- scripts/shaTest.php
+++ scripts/shaTest.php
@@ -1,3 +1,11 @@
+/**
+ * Login with Amazon - SHA 256 compatibility test scripts
+ *
+ * @category    Amazon
+ * @package     shaTest.php
+ * @copyright   Copyright (c) 2016 Amazon.com
+ * @license     http://opensource.org/licenses/Apache-2.0  Apache License, Version 2.0
+ */
 <?php
     // create curl resource
     $ch = curl_init();

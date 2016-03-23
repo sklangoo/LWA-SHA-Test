6213677908709fdd80c145e49a2b77ed8a3ffc3e
sklangoo <langoos@amazon.com>
2016-03-23 12:46:27 -0700

    Update shaTest.py

--- scripts/shaTest.py
+++ scripts/shaTest.py
@@ -1,3 +1,11 @@
+/**
+ * Login with Amazon - SHA 256 compatibility test scripts
+ *
+ * @category    Amazon
+ * @package     shaTest.py
+ * @copyright   Copyright (c) 2016 Amazon.com
+ * @license     http://opensource.org/licenses/Apache-2.0  Apache License, Version 2.0
+ */
 import urllib2
 
 try:
